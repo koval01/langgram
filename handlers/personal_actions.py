2782c94b86d62d42578c69e1e751e243c8870e58
Koval Yaroslav <yaroslav@koval.page>
2023-10-15 20:42:06 +0300

    Update handlers package

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -2,8 +2,6 @@ from aiogram import types
 from dispatcher import dp
 
 
-# Personal actions goes here (bot direct messages)
-# Here is some example !ping command ...
 @dp.message_handler(is_owner=True, commands="ping", commands_prefix="!/")
 async def cmd_ping_bot(message: types.Message):
-    await message.reply("<b>👊 Up & Running!</b>\n\n")
+    await message.reply("<b>Up & Running!</b>")
